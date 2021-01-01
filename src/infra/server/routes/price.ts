60b94921a8008a389a154ebd1acb9dcd4d353197
Rafael Arantes <birutaibm@gmail.com>
2021-01-01 08:24:47 -0300

    Fix mistake merging branches

--- src/infra/server/routes/price.ts
+++ src/infra/server/routes/price.ts
@@ -2,11 +2,14 @@ import { Router } from 'express';
 
 import { routeAdapter } from '@infra/server/express-router';
 import { LoadLastPriceController } from '@presentation/controllers';
-import { makeLoadLastPriceController } from '@main/factories';
+import { ControllerFactory } from '@presentation/factories';
 import { tickerFromRoute } from '@infra/server/express-input-adapter';
 
-export default function (router: Router): void {
-  const controller: LoadLastPriceController = makeLoadLastPriceController();
+export default function (
+  router: Router,
+  controllerFactory: ControllerFactory<LoadLastPriceController>
+): void {
+  const controller = controllerFactory.make();
   router.get(
     '/price/last/:ticker',
     routeAdapter.adaptWith(controller, tickerFromRoute)
